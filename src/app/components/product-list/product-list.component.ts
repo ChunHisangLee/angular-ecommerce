ce7c52d68a9093b145d792f4a5b44bac7d258b8f
ChunHisangLee <goldrushkl168@gmail.com>
2024-07-26 23:36:26 +0800

    2024/07/26  create product calss and Product Service

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,16 +1,33 @@
 import {Component, OnInit} from '@angular/core';
+import {Product} from "../../common/product";
+import {ProductService} from "../../services/product.service";
+import {CurrencyPipe, NgForOf} from "@angular/common";
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [],
+  imports: [
+    CurrencyPipe,
+    NgForOf
+  ],
   templateUrl: './product-list.component.html',
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent implements OnInit {
-  constructor() {
+  products: Product[] = [];
+
+  constructor(private productService: ProductService) {
   }
 
   ngOnInit() {
+    this.listProducts();
+  }
+
+  listProducts() {
+    this.productService.getProductList().subscribe(
+      data => {
+        this.products = data;
+      }
+    )
   }
 }
